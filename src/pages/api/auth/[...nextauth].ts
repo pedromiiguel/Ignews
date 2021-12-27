8d5da77d463e6a66d9a7d1be447d7d1973f2a46f
Pedro Miguel <pedro.araujo@supera.com.br>
2021-12-27 15:03:59 -0300

    finish application

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,72 @@
+import { query } from 'faunadb';
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, token }) {
+     try {
+      const userActiveSubscription = await fauna.query(
+        query.Get(
+          query.Intersection([
+            query.Match(
+              query.Index('subscription_by_user_ref'),
+              query.Select(
+                'ref',
+                query.Get(
+                  query.Match(
+                    query.Index('user_by_email'),
+                    query.Casefold(session.user.email),
+                  ),
+                ),
+              ),
+            ),
+            query.Match(query.Index('subscription_by_status'), 'active'),
+          ]),
+        ),
+      );
+      return { ...session, activeSubscription: userActiveSubscription };
+     } catch (error) {
+      return {...session, activeSubscription: null}
+     }
+
+    },
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(user.email),
+                ),
+              ),
+            ),
+            query.Create(query.Collection('users'), { data: { email } }),
+
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(user.email),
+              ),
+            ),
+          ),
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,89 @@
+import Prismic from '@prismicio/client';
+import { GetStaticProps } from 'next';
+import { useSession } from 'next-auth/react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updateAt: string;
+};
+
+type PostProps = {
+  posts: Post[];
+};
+
+export default function Posts({ posts }: PostProps) {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map((post) => (
+            <Link
+              key={post.slug}
+              href={
+                session?.activeSubscription?.data?.status === 'active'
+                  ? `posts/${post.slug}`
+                  : `posts/preview/${post.slug}`
+              }
+            >
+              <a>
+                <time>{post.updateAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 100,
+    },
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'paragraph')
+          ?.text ?? '',
+      updateAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: 'numeric',
+          month: 'long',
+          year: 'numeric',
+        },
+      ),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
